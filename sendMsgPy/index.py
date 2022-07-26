7b896602ccaed8078be7e4a77efb2afe59a30620
Camieux <72339948+camieuxgoofy@users.noreply.github.com>
2022-07-26 23:08:35 +0700

    Update index.py
    Input is still manually there is an image url, text and buttons

--- sendMsgPy/index.py
+++ sendMsgPy/index.py
@@ -4,4 +4,8 @@ api_key = 'bot token'
 user_id = 'user ID'
 
 bot = telegram.Bot(token=api_key)
-bot.send_message(chat_id=user_id, text='USP-Python has started up!')
+
+bot.send_photo(chat_id=user_id, photo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqtU1UzVIP7PA7FoiLTXXdosETBSLG_sxLdqWJqgeXkZ60kLE15ochjtOy&s=10", caption="#k-on \nK-ON Test.", reply_markup=telegram.InlineKeyboardMarkup ( [
+        [telegram.InlineKeyboardButton(text='on Facebook', url='https://facebook.com')],
+        [telegram.InlineKeyboardButton(text='on Telegram', url='https://t.me')],
+    ] ))
