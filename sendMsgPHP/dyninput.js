2bd91a4caf0e5497e34b267329e402477287decf
Camieux <tarsisiusadhi@gmail.com>
2022-08-24 22:58:07 +0700

    bruh

--- sendMsgPHP/dyninput.js
+++ sendMsgPHP/dyninput.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var max_fields = 10;
     var add_input_button = $('.add_input_button');
     var field_wrapper = $('.field_wrapper');
-    var new_field_html = '<div> <input placeholder="Text Button" type="text" class="custom-input-field" name="text_btn[]" /> <input placeholder="Url Button" type="text" class="custom-input-field float-right" name="url_btn[]" /> <a href="javascript:void(0);" class = "remove_input_button" title = "Remove field"> <img src ="remove-icon.png"/> </a> </div > ';
+    var new_field_html = '<div class="form-row"> <div class="form-group col-6"> <input placeholder="Text Button" type="text" class="custom-input-field" name="text_btn" /> </div> <div class="form-group col-6"> <input placeholder="Url Button" type="text" class="custom-input-field" name="url_btn" /> <a href="javascript:void(0);" class="remove_input_button" title="Remove Field"><img src="remove-icon.png" /></a> </div> </div>';
     var input_count = 1;
     // Add button dynamically
     $(add_input_button).click(function () {
@@ -14,7 +14,7 @@ $(document).ready(function () {
     // Remove dynamically added button
     $(field_wrapper).on('click', '.remove_input_button', function (e) {
         e.preventDefault();
-        $(this).parent('div').remove();
+        $(this).parent().parent('div').remove();
         input_count--;
     });
 });
--- sendMsgPHP/index.php
+++ sendMsgPHP/index.php
@@ -1,37 +1,55 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="dyninput.js"></script>
 </head>
 
 <body>
-    <form action="" method="post">
-        <div class="field_wrapper">
-            <input placeholder="Image URL" type="text" name="photo" />
-            <input placeholder="Caption" type="text" name="caption" />
-            <div>
-                <input placeholder="Text Button" type="text" class="custom-input-field" name="text_btn" />
-                <input placeholder="Url Button" type="text" class="custom-input-field float-right" name="url_btn" />
-                <a href="javascript:void(0);" class="add_input_button" title="Add field"><img src="add-icon.png" /></a>
+
+    <div class="container d-flex justify-content-center">
+        <form method="post">
+            <div class="field_wrapper">
+                <div class="form-row">
+                    <div class="form-group col-6">
+                        <input placeholder="Image URL" type="text" name="photo" />
+                    </div>
+                    <div class="form-group col-6">
+                        <input placeholder="Caption" type="text" name="caption" />
+                    </div>
+                </div>
+                <!-- <div class="form-row">
+                    <div class="form-group col-6">
+                        <input placeholder="Text Button" type="text" class="custom-input-field" name="text_btn" />
+                    </div>
+                    <div class="form-group col-6">
+                        <input placeholder="Url Button" type="text" class="custom-input-field" name="url_btn" />
+                    </div>
+                </div> -->
             </div>
-        </div>
-        <input type="submit" name="submit" />
-    </form>
+            <input class="btn btn-info" type="submit" name="submit" />
+            <a href="javascript:void(0);" class="add_input_button" title="Add field"><img src="add-icon.png" /></a>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
 
 <?php
 if (isset($_POST['submit'])) {
-    $apiToken = "1924018403:AAGozbPhM0kY2stYq5M2bcNw7RUUlloYmUs";
+    $apiToken = "...";
     $data = http_build_query([
-        'chat_id' => '908060423',
+        'chat_id' => '...',
         'photo' => $_POST['photo'],
         'caption' => $_POST['caption'],
         'parse_mode' => 'HTML'
@@ -47,7 +65,7 @@ if (isset($_POST['submit'])) {
             ]
         ]
     ]);
-    // Send keyboard
+    // Send messages
     $url = "https://api.telegram.org/bot$apiToken/sendPhoto?{$data}&reply_markup={$keyboard}";
     $res = @file_get_contents($url);
 }
